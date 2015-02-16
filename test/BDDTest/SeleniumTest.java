711cb55f786ddb1203a124398db297461ffafce0
Guilherme <guilherme.prestes@gmail.com>
2015-02-16 15:33:32 -0800

    add SeleniumTest class

---
+++ test/BDDTest/SeleniumTest.java
@@ -0,0 +1,34 @@
+package BDDTest;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SeleniumTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private String browserName;
+    private String browserVersion;
+
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "http://www.dunelm-mill.com/";
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
+        browserName = caps.getBrowserName();
+        browserVersion = caps.getVersion();
+        System.out.println("Automated test run.We're running on " + browserName + "" + browserVersion);
+    }
+
+    public void tearDown() {
+        driver.quit();
+    }
+
+    public void goToHomePage() {
+        driver.get(baseUrl);
+    }
+}
