846d74a9537f2b4140f6888a38e74f530f9d3e2c
Guilherme <guilherme.prestes@gmail.com>
2015-02-16 15:41:07 -0800

    add pending steps to StepDefs

--- test/BDDTest/StepDefs.java
+++ test/BDDTest/StepDefs.java
@@ -1,5 +1,10 @@
 package BDDTest;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class StepDefs {
     private SeleniumTest script;
 
@@ -11,5 +16,23 @@ public class StepDefs {
 
     public void tidyUp() {
         script.tearDown();
+    }
+
+    @Given("^I am on the homepage$")
+    public void i_am_on_the_homepage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I search for pillows$")
+    public void i_search_for_pillows() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the page title is as expected$")
+    public void the_page_title_is_as_expected() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
     }
 }
