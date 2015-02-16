08c598452dd6fad538ab4b24ed1e19401f780601
Guilherme <guilherme.prestes@gmail.com>
2015-02-16 15:32:07 -0800

    add cukeRunner

---
+++ test/BDDTest/CukeRunner.java
@@ -0,0 +1,16 @@
+package BDDTest;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"test/"},
+        plugin = {"pretty",
+                "html:target/cucumber-html-report",
+                "junit:target/cucumber-junit-report/allcukes.xml"},
+        tags = {"@Runme"}
+)
+public class CukeRunner {
+}
