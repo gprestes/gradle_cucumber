13ac9f37025d6ee0dced2736e2cc3b6e8c2cf511
Guilherme <guilherme.prestes@gmail.com>
2015-02-16 15:34:13 -0800

    add initial StepDefs class

---
+++ test/BDDTest/StepDefs.java
@@ -0,0 +1,15 @@
+package BDDTest;
+
+public class StepDefs {
+    private SeleniumTest script;
+
+    public void setUpWebDriver() throws Exception {
+        script = new SeleniumTest();
+        script.setUp();
+        script.goToHomePage();
+    }
+
+    public void tidyUp() {
+        script.tearDown();
+    }
+}
