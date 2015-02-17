6effe97201dfc4e806fa97ac80c457fc5ec96f9b
Guilherme <guilherme.prestes@gmail.com>
2015-02-16 19:14:19 -0800

    add support to chromedriver and refactor call to proper driver

--- test/BDDTest/SeleniumTest.java
+++ test/BDDTest/SeleniumTest.java
@@ -3,11 +3,10 @@ package BDDTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -21,27 +20,29 @@ public class SeleniumTest {
     private String browserVersion;
 
     public void setUp() throws Exception {
-        System.setProperty("webdriver", "ie");
+        System.setProperty("webdriver", "chrome");
 
-        if ("headless".equals(System.getProperty("webdriver"))) {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setJavascriptEnabled(true);
-            capabilities.setCapability(
-                    PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-                    "/drivers/phantomjs.exe"
-            );
-            driver = new PhantomJSDriver(capabilities);
-        }
-        if ("ie".equals(System.getProperty("webdriver"))) {
-            System.setProperty("webdriver.ie.driver", "/drivers/IEDriverServer.exe");
-            driver = new InternetExplorerDriver();
-        } else {
-            driver = new FirefoxDriver();
-            driver.manage().window().maximize();
+        switch (System.getProperty("webdriver")) {
+            case "headless":
+                System.setProperty("phantomjs.binary.path", "/drivers/phantomjs.exe");
+                driver = new PhantomJSDriver();
+                break;
+            case "ie":
+                System.setProperty("webdriver.ie.driver", "/drivers/IEDriverServer.exe");
+                driver = new InternetExplorerDriver();
+                break;
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "/drivers/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+            default:
+                driver = new FirefoxDriver();
         }
 
         baseUrl = "http://www.dunelm-mill.com/";
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
         Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
         browserName = caps.getBrowserName();
